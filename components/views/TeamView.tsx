78787eb466df685d3486dea033cbde36ab652891
cessuofk <cess.uofk.sd@gmail.com>
2026-09-06 20:07:10 +0300

    Update TeamView.tsx

--- components/views/TeamView.tsx
+++ components/views/TeamView.tsx
@@ -410,7 +410,7 @@ export function TeamView({ cardCovers }: TeamViewProps) {
                                 style={{
                                   width: '100%',
                                   aspectRatio: '4 / 4.6',
-                                  maxHeight: '25rem',
+                                  Height: '25rem',
                                   background: 'var(--surface)',
                                   display: 'flex',
                                   alignItems: 'center',
